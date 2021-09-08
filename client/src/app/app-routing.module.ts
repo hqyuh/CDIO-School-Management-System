48e4c9a54eab659369e7122e84b22899ee2780e7
Pupu <ldhuy0102work@gmail.com>
2021-09-08 19:14:08 +0700

    add login to account route

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'account',
+    loadChildren: ()=> import('./module/account/account.module').then(m=>m.AccountModule),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'account'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/module/account/account-routing.module.ts
+++ client/src/app/module/account/account-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'login'
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
