c964c6642186abe115c8ff612e9ce6b7ff3099d6
Pupu <ldhuy0102work@gmail.com>
2021-09-08 19:19:48 +0700

    add router outlet

--- client/src/app/module/account/account-routing.module.ts
+++ client/src/app/module/account/account-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
   {
@@ -10,6 +11,11 @@ const routes: Routes = [
         path: 'login',
         component: LoginComponent
       },
+      {
+        path: 'register',
+        component: RegisterComponent
+      }
+      ,
       {
         path: '',
         pathMatch: 'full',
