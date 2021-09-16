6c121fc2eb44242452ae3d9138e8d9215c6c78d3
Pupu <ldhuy0102work@gmail.com>
2021-09-16 10:40:15 +0700

    Add register form validation

--- client/src/app/module/account/pages/register/register.component.ts
+++ client/src/app/module/account/pages/register/register.component.ts
@@ -27,13 +27,10 @@ export class RegisterComponent implements OnInit {
         '',
         [
           Validators.required,
-          Validators.minLength(6),
-          Validators.maxLength(18),
-          Validators.pattern('^[a-zA-Z0-9!@#$%^&*]{6,18}$'),
         ],
       ],
     },{
-      validators: this.passwordsMatch()
+      validator: this.passwordsMatch()
     });
   }
 
@@ -43,7 +40,9 @@ export class RegisterComponent implements OnInit {
 
   public passwordsMatch() {
     return (form: FormGroup): { [s: string]: boolean } | null => {
-      if (form.controls.password.value !== form.controls.confirmPassword.value) {
+      const password = form.controls.password?.value;
+      const confirmPassword = form.controls.confirmPassword.value;
+      if (password !== confirmPassword) {
         return { 'misMatch': true };
       } else {
         return null;
