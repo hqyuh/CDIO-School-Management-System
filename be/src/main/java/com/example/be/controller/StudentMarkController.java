9c95c05c948c93fdbef4e8480936955004d2d891
OS <hoquanghuy0123@gmail.com>
2021-10-28 20:52:24 +0700

    fix

--- be/src/main/java/com/example/be/controller/StudentMarkController.java
+++ be/src/main/java/com/example/be/controller/StudentMarkController.java
@@ -3,7 +3,6 @@ package com.example.be.controller;
 import com.example.be.dto.StudentMarkDTO;
 import com.example.be.service.StudentMarkService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
