3480f6d05c16b7726d306fc7fc542b2a48229508
OS <hoquanghuy0123@gmail.com>
2022-03-24 23:28:02 +0700

    [Update]: corsFilter

--- be/src/main/java/com/example/server/BeApplication.java
+++ be/src/main/java/com/example/server/BeApplication.java
@@ -2,14 +2,8 @@ package com.example.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
-import java.util.Arrays;
-import java.util.Collections;
 
 @SpringBootApplication
 @EnableAsync
